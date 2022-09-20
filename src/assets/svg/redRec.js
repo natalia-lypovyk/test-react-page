aa7db5bf7ea0098e6902889644c65ea09ed4eee9
Natalia Lypovyk <cardinnata@gmail.com>
2022-09-20 14:52:08 +0300

    correct code style (assets directory)

--- src/assets/svg/redRec.js
+++ src/assets/svg/redRec.js
@@ -1,15 +1,18 @@
-import * as React from "react"
-
 const RedRec = (props) => (
-    <svg
-        {...props}
-        width="8px"
-        height="8px"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-    >
-        <rect width={8} height={8} rx={2} fill="#FF4259" />
-    </svg>
-)
+  <svg
+    {...props}
+    width="8px"
+    height="8px"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect
+      width={8}
+      height={8}
+      rx={2}
+      fill="#FF4259"
+    />
+  </svg>
+);
 
-export default RedRec
+export default RedRec;
