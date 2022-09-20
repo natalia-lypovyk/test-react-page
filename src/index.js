eacd6c0481a72cb35b68334d9e3fdd53c1ab5663
Natalia Lypovyk <cardinnata@gmail.com>
2022-09-20 14:50:37 +0300

    correct render in react18

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import React from "react";
-import ReactDOM from 'react-dom';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 
-import App from "./screens/App/app";
+import App from './screens/App/app';
 import './index.css';
 
-ReactDOM.render(<App/>, document.getElementById(('root')))
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
