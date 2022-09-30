e89828e74d251714a4fbd4dd6881443ef58fed91
Natalia Lypovyk <cardinnata@gmail.com>
2022-09-30 15:08:49 +0300

    remove mixed content error possible fix

--- src/utils/get-data.js
+++ src/utils/get-data.js
@@ -3,8 +3,6 @@ export const allFarmsUrl = 'http://95.216.162.93:8000/getAllFarms';
 export const farmsBySearchUrl = 'http://95.216.162.93:8000/getFarmsBySearch';
 
 export const getData = async (query) => {
-  const response = await fetch(query, { referrerPolicy: "unsafe-url" });
+  const response = await fetch(query);
   return response.json();
 };
-
-
