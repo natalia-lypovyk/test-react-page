e8a819f90ceb045e3db2141006ab6027a4899b53
Natalia Lypovyk <cardinnata@gmail.com>
2022-10-15 23:36:23 +0300

    correct Search icon size units

--- src/assets/svg/search.js
+++ src/assets/svg/search.js
@@ -1,8 +1,8 @@
 const Search = (props) => (
   <svg
     {...props}
-    width="24em"
-    height="24em"
+    width="24"
+    height="24"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
