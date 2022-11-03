0549e5551dee5578db95a2368739ac91d4ad6362
Natalia Lypovyk <cardinnata@gmail.com>
2022-11-03 20:42:01 +0200

    separate dropdown

---
+++ src/components/Dropdown/dropdown.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+export const Dropdown = ({ selectedValue, setSelectedValue, configs }) => {
+  const [isOpenDropdown, setOpenDropdown] = useState(false);
+
+  const handleOpen = () => {
+    setOpenDropdown(!isOpenDropdown);
+  };
+
+  const onOptionClicked = (value) => {
+    setSelectedValue(value);
+    setOpenDropdown(false);
+  };
+
+  return (
+    <div className="dropdown">
+      <div
+        className="dropdown__header"
+        onClick={handleOpen}
+      >
+        {selectedValue.name || 'Choose config'}
+      </div>
+
+      {isOpenDropdown ? (
+        <ul className="dropdown__list">
+          {configs && configs.map((config) => (
+            <li
+              key={config.name}
+              className="dropdown__item"
+              onClick={() => onOptionClicked(config)}
+            >
+              {config.name}
+            </li>
+          ))}
+        </ul>
+      ) : null}
+    </div>
+  );
+};
